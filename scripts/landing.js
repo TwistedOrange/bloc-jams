2fafe2b0c5326484a08b3ef7def1f2d3ff7fe669
Deb Strickland <debfrombc@gmail.com>
2017-04-21 11:20:44 -0700

    renamed animate func

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,14 @@
 var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+  var ptNode = document.getElementsByClassName('point');
 
-  function revealPoint(ptIndx) {
-   var ptNode = document.getElementsByClassName('point');
-
-   function showPoint(ptIdx) {
-     ptNode[ptIdx].style.opacity = 1;
-     ptNode[ptIdx].style.transform = "scaleX(1) translateY(0)";
-     ptNode[ptIdx].style.msTransform = "scaleX(1) translateY(0)";
-     ptNode[ptIdx].style.WebkitTransform = "scaleX(1) translateY(0)";
-   }
+  var revealPoint = function(i) {
+    ptNode[i].style.opacity = 1;
+    ptNode[i].style.transform = "scaleX(1) translateY(0)";
+    ptNode[i].style.msTransform = "scaleX(1) translateY(0)";
+    ptNode[i].style.WebkitTransform = "scaleX(1) translateY(0)";
   }
 
-   for (var i=0; i<ptNode.length; i++) {
-     revealPoint(i);
-   }
+  for (var i=0; i<ptNode.length; i++) {
+   revealPoint(i);
+  }
 };
