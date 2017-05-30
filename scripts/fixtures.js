8caf7ca281d18b6580e3ee9905915c2b6289010f
Deb Strickland <debfrombc@gmail.com>
2017-05-29 21:26:31 -0700

    changed songs

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -1,15 +1,16 @@
+// source: https://archive.org/details/OTRR_An_Evening_With_Groucho_Singles
 var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: 'assets/images/album_covers/01.png',
+  title: 'Old Time Radio',
+  artist: 'Grocho Marx',
+  label: '',
+  year: 'Oct 1890 - Aug 1977',
+  albumArtUrl: 'assets/images/album_covers/groucho.jpg',
   songs: [
-    { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
-    { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
-    { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
-    { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
-    { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }
+    { title: 'Toronto Song', duration: '1:35', audioUrl: 'assets/music/toronto' },
+    { title: 'Uncle Julius', duration: '1:37', audioUrl: 'assets/music/unclejulius' },
+    { title: 'Houdini Story', duration: '1:17', audioUrl: 'assets/music/houdini' },
+    { title: 'How I Got Started', duration: '0:55', audioUrl: 'assets/music/started' },
+    { title: 'Hello, I Must Be Going', duration: '0:42', audioUrl: 'assets/music/going' }
   ]
 };
 
