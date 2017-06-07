1d097eb675b1ddb5454cffbd7bff1d32ff269d81
Deb Strickland <debfrombc@gmail.com>
2017-06-06 21:52:21 -0700

    added length field to album structure

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -4,19 +4,12 @@ var albumPicasso_OLD = {
   label: 'Cubism',
   year: '1881',
   albumArtUrl: 'assets/images/album_covers/01.png',
-  // songs: [
-  //   { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
-  //   { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
-  //   { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
-  //   { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
-  //   { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }
-  // ]
   songs: [
-    { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-    { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-    { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-    { title: 'Pink', duration: 153.45, audioUrl: 'assets/music/pink' },
-    { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+    { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
+    { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
+    { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
+    { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
+    { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }
   ]
 };
 
@@ -35,13 +28,13 @@ var albumPicasso = {
   //   { title: "Gilligan's Island", duration: '1:31', audioUrl: 'assets/music/gilligan' },
   //   { title: 'Batman', duration: '0:43', audioUrl: 'assets/music/batman' }
   // ]
-  // change to seconds
+  // change to seconds, added new field 'length' for actual song length to display
   songs: [
-    { title: 'Green Acres', duration: 161.71, audioUrl: 'assets/music/grnacres' },
-    { title: 'Andy Griffith Show', duration: 103.95, audioUrl: 'assets/music/andgrif' },
-    { title: 'Cheers', duration: 268.45, audioUrl: 'assets/music/cheers' },
-    { title: "Gilligan's Island", duration: 153.45, audioUrl: 'assets/music/gilligan' },
-    { title: 'Batman', duration: 374.22, audioUrl: 'assets/music/batman' }
+    { title: 'Green Acres', length: '1:04', duration: 161.71, audioUrl: 'assets/music/grnacres' },
+    { title: 'Andy Griffith Show', length: '0:50', duration: 103.95, audioUrl: 'assets/music/andgrif' },
+    { title: 'Cheers', length: '1:03', duration: 268.45, audioUrl: 'assets/music/cheers' },
+    { title: "Gilligan's Island", length: '1:31', duration: 153.45, audioUrl: 'assets/music/gilligan' },
+    { title: 'Batman', length: '0:43', duration: 374.22, audioUrl: 'assets/music/batman' }
   ]
 };
 
